fd2967a4fb5a17b1f11eb8fadbe82e3a82054407
Mariana Videira <88859600+MarianaVideira@users.noreply.github.com>
2023-06-17 00:38:06 +0100

    new updates.

--- 3.SFA_CSH.R
+++ 3.SFA_CSH.R
@@ -6,6 +6,9 @@ library(ggplot2)
 library(xtable)
 library(plm)
 library(stargazer)
+library(purrr)
+library(stringr)
+
 
 # 1. Data -----------------------------------------------------------------
 load("0.DataBase/CSH.RData")
@@ -13,6 +16,8 @@ load("0.DataBase/CSH.RData")
   setwd("3.SFA_outputs")
   dir.create("CSH")
   setwd("CSH")
+  dir.create("1. Portugal Frontier (operational)")
+  setwd("1. Portugal Frontier (operational)")
   ## 1.1 Variables and Functional form ------------------------------------
     ### A. Select variables and test correlation ----------------------------
     CSH_0 = CSH |> select(hospital, year, operational, cmvmc, in_days, RO, azo, region, 
@@ -32,7 +37,7 @@ load("0.DataBase/CSH.RData")
       filter(azo == 0) |>
       drop_na()
     
-    ### B. Panel data -------------------------------------------------------
+    ### B. Panel data -----------------------------------------------------
     CSH_panel = pdata.frame(CSH_0, c("hospital", "year"))
   ## 1.2 Define functional forms ------------------------------------------
   form = log(operational) ~ log(in_days) + log(surg) + log(urge) + log(RO) # includes Dummy variable
@@ -48,7 +53,7 @@ load("0.DataBase/CSH.RData")
   form_r = log(operational) ~ log(in_days) + log(surg) + log(urge) + log(RO) + factor(region) 
   formt_r = log(operational) ~ log(in_days) + log(surg) + I((log(in_days)^2)/2) + I((log(surg)^2)/2) + log(urge) + log(RO) + factor(region) 
   
-# 2. Operational Cost function --------------------------------------------
+# 2. Operational Cost function : Continent --------------------------------
   ## 2.1 Estimation: time invariant inefficiencies ------------------------
     ### A. Cobb-Douglas (form) ----------------------------------------------
     sfront = sfa( form,  
@@ -198,12 +203,277 @@ load("0.DataBase/CSH.RData")
                                  "$\\log$ urgencies", "$(\\log \\text{inpatient days}) ^2$", 
                                  "$\\log \\text{surgeries} ^2$", 
                                  "Z Constant", "Rate of Occupancy", "Wait for Scheduled Surgeries"))
-
   ### C. G05 --------------------------------------------------------------
 ## STOPED HERE -------
+# 4. Azores Prediction: Continent Frontier --------------------------------
+  ## 4.1 Prediction Portugal Continent ---------------------------------------
+  CSH_predict = CSH_panel |> select(hospital, year, operational)
+  
+  # A. prediction tables
+  prediction_sfa = data.frame(predict = predict(sfront, newdata = CSH_panel),
+                              hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predict_sfa" = "predict")
+  prediction_sfat = data.frame(predict = predict(sfrontt, newdata = CSH_panel),
+                               hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predict_sfat" = "predict")
+  prediction_sfaz = data.frame(predict = predict(sfrontz, newdata = CSH_panel),
+                               hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predict_sfaz" = "predict")
+  prediction_sfatz = data.frame(predict = predict(sfronttz, newdata = CSH_panel),
+                                 hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predict_sfatz" = "predict")
+  
+  # B. Join all data frames 
+  list_df = list(CSH_predict, prediction_sfa,
+                 prediction_sfat, prediction_sfaz, prediction_sfatz)
+  
+  CSH_predict = list_df |> 
+    reduce(function(x, y) full_join(x, y, by = c("hospital" = "hospital",
+                                                 "year" = "year"), 
+                                    .init = NULL))
+  
+  ## 4.2 Prediction Azores Island -----------------------------------------
+  CSH_panel_a = CSH |> select(hospital, year, operational, cmvmc, in_days, RO, azo, region, 
+                        surg, app, urge, patient_dis, wait_scheduled_surg) |> 
+    filter(azo ==1) |> drop_na() |> pdata.frame(c("hospital", "year"))
+  
+  CSH_predict_a = CSH_panel_a |> select(hospital, year, operational) 
 
+  # A. prediction tables
+  prediction_sfa = data.frame(predict = exp(predict(sfront, newdata = CSH_panel_a)),
+                              hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel_a)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-")
+  prediction_sfat = data.frame(predict = exp(predict(sfrontt, newdata = CSH_panel_a)),
+                              hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel_a)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predictt" = "predict")
+  prediction_sfaz = data.frame(predict = exp(predict(sfrontz, newdata = CSH_panel_a)),
+                              hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel_a)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predictz" = "predict")
+  prediction_sfatz = data.frame(predict = exp(predict(sfronttz, newdata = CSH_panel_a)),
+                              hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel_a)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predicttz" = "predict")
+  
+  # B. Join all data frames 
+  list_df = list(CSH_predict_a, prediction_sfa,
+                 prediction_sfat, prediction_sfaz, prediction_sfatz)
+  
+  CSH_predict_a = list_df |> 
+    reduce(function(x, y) full_join(x, y, by = c("hospital" = "hospital",
+                                                 "year" = "year"), 
+                                    .init = NULL))
+  # C. Differences
+  # These differences are too high
+  CSH_predict_a = CSH_predict_a |> mutate(difference = operational - predict, differencet = operational - predictt,
+                                          differencez = operational - predictz, differencetz = operational - predicttz)
+  
+  ## 4.3 Efficiency Portugal Continent ------------------------------------
+  # A. Efficiency tables
+  efficiencies_sfa = data.frame(hospital = rownames(efficiencies(sfront, newdata = CSH_panel)),
+                                efficiency = efficiencies(sfront, newdata = CSH_panel)) |>
+    rename("efficiency_sfa" = "efficiency")
+  efficiencies_sfat = data.frame(efficiency = efficiencies(sfrontt, newdata = CSH_panel),
+                                 hospital = rownames(efficiencies(sfrontt, newdata = CSH_panel))) |>
+    rename("efficiency_sfat" = "efficiency")
+  efficiencies_sfaz = data.frame(efficiency = efficiencies(sfrontz, newdata = CSH_panel),
+                                 hospital = rownames(efficiencies(sfrontz, newdata = CSH_panel))) 
+  efficiencies_sfatz = data.frame(efficiency = efficiencies(sfronttz, newdata = CSH_panel),
+                                  hospital = rownames(efficiencies(sfronttz, newdata = CSH_panel))) |>
+    rename("efficiencyt.2015" = "efficiency.2015", "efficiencyt.2016" = "efficiency.2016",
+           "efficiencyt.2017" = "efficiency.2017", "efficiencyt.2018" = "efficiency.2018",
+           "efficiencyt.2019" = "efficiency.2019", "efficiencyt.2020" = "efficiency.2020",
+           "efficiencyt.2021" = "efficiency.2021")
+
+  # B. Join all efficiency tables
+  list_df = list(efficiencies_sfa, efficiencies_sfat,
+                 efficiencies_sfaz, efficiencies_sfatz)
+  
+  CSH_effi = list_df |> 
+    reduce(function(x, y) full_join(x, y, by = c("hospital" = "hospital"), 
+                                    .init = NULL))
+  
+  # C. Descriptive 
+  CSH_effi = CSH_effi |> mutate(mean_efficiencyz = rowMeans(CSH_effi[,4:10], na.rm = TRUE),
+                                mean_efficiencyz = rowMeans(CSH_effi[,11:16], na.rm = TRUE))
+  ## 4.4 Efficiency Azores Island -----------------------------------------
+  
+  # A. Efficiency tables
+  efficiencies_sfa = data.frame(efficiency = efficiencies(sfront, newdata = CSH_panel_a),
+                                hospital = rownames(efficiencies(sfront, newdata = CSH_panel_a))) |>
+    rename("efficiency_sfa" = "efficiency")
+  efficiencies_sfat = data.frame(efficiency = efficiencies(sfrontt, newdata = CSH_panel_a),
+                                 hospital = rownames(efficiencies(sfrontt, newdata = CSH_panel_a))) |>
+    rename("efficiency_sfat" = "efficiency")
+  efficiencies_sfaz = data.frame(efficiency = efficiencies(sfrontz, newdata = CSH_panel_a),
+                                 hospital = rownames(efficiencies(sfrontz, newdata = CSH_panel_a))) 
+  efficiencies_sfatz = data.frame(efficiency = efficiencies(sfronttz, newdata = CSH_panel_a),
+                                  hospital = rownames(efficiencies(sfronttz, newdata = CSH_panel_a))) |>
+    rename("efficiencyt.2015" = "efficiency.2015", "efficiencyt.2016" = "efficiency.2016",
+           "efficiencyt.2017" = "efficiency.2017", "efficiencyt.2018" = "efficiency.2018",
+           "efficiencyt.2019" = "efficiency.2019", "efficiencyt.2020" = "efficiency.2020",
+           "efficiencyt.2021" = "efficiency.2021")
+  
+  # B. Join all efficiency tables
+  list_df = list(efficiencies_sfa, efficiencies_sfat,
+                 efficiencies_sfaz, efficiencies_sfatz)
+  
+  CSH_effi_a = list_df |> 
+    reduce(function(x, y) full_join(x, y, by = c("hospital" = "hospital"), 
+                                    .init = NULL))
+  # C. Descriptive 
+  CSH_effi = CSH_effi |> mutate(mean_efficiencyz = rowMeans(CSH_effi[,4:10], na.rm = TRUE),
+                                mean_efficiencyz = rowMeans(CSH_effi[,11:16], na.rm = TRUE))
+  
+# 5. Data ------------------------------------------------------------------
+rm(list = setdiff(ls(), "CSH"))
+setwd("..")
+  dir.create("2. Frontier w Azores Dummy (operational)")
+  setwd("2. Frontier w Azores Dummy (operational)")
+  
+  ### A. Select variables and test correlation ----------------------------
+  CSH_panel = CSH |> select(hospital, year, operational, cmvmc, in_days, RO, azo, region, 
+                        surg, urge, patient_dis, wait_scheduled_surg) |> 
+    drop_na() |>
+    pdata.frame(c("hospital", "year"))
+  
+# 6. Operational Cost function: Azores Dummy ------------------------------
+  #(choosen the scale variable will capture the effects of the others)
+  CSH_0 = CSH |> select(hospital, year, operational, in_days, RO, azo, region, 
+                        surg, urge, wait_scheduled_surg) |> 
+    drop_na()
+  
+  ### A. Panel data -------------------------------------------------------
+  CSH_panel = pdata.frame(CSH_0, c("hospital", "year"))
+  ## 6.1 Define functional forms ------------------------------------------
+  form = log(operational) ~ log(in_days) + log(surg) + log(urge) + log(RO) + azo
+  formt = log(operational) ~ log(in_days) + log(surg) + I((log(in_days)^2)/2) + I((log(surg)^2)/2) + log(urge) + log(RO) + azo
+  
+  # Firm level inefficiencies models BC95
+  # mean inefficiency of u_it is "determined" by factors z_it
+  formz = log(operational) ~ log(in_days) + log(surg) + log(urge) + azo| RO + wait_scheduled_surg
+  formtz = log(operational) ~ log(in_days) + log(surg) + log(urge) + I((log(in_days)^2)/2) + I((log(surg)^2)/2) + azo| RO + wait_scheduled_surg
+  
+  ## 6.2. Operational Cost function ----------------------------------------
+  ### Estimation: time invariant inefficiencies ----------------------------
+  ### A. Cobb-Douglas (form) -----------------------------------------------
+  sfront = sfa( form,  
+                data = CSH_panel,
+                ineffDecrease = F, # FALSE for cost function and TRUE for production
+                truncNorm = FALSE, # FALSE -> errors have half normal distribution TRUE -> truncated distribution (mu parameter is added)
+                timeEffect = FALSE, # time is allowed to have an effect on efficiency
+                printIter = 1 )
+  summary(sfront, extraPar = TRUE)
+  
+  ### B. Trans log (formt) -------------------------------------------------
+  sfrontt = sfa( formt,  
+                 data = CSH_panel,
+                 ineffDecrease = F, # FALSE for cost function and TRUE for production
+                 truncNorm = FALSE, # FALSE -> errors have half normal distribution TRUE -> truncated distribution (mu parameter is added)
+                 timeEffect = FALSE, # time is allowed to have an effect on efficiency
+                 printIter = 1 )
+  summary(sfrontt)
+  
+  ### C. Cobb-Douglas with z_it (formz) -------------------------------------
+  sfrontz = sfa( formz,  
+                 data = CSH_panel,
+                 ineffDecrease = F, # FALSE for cost function and TRUE for production
+                 truncNorm = FALSE, # FALSE -> errors have half normal distribution TRUE -> truncated distribution (mu parameter is added)
+                 timeEffect = FALSE, # time is allowed to have an effect on efficiency
+                 printIter = 1 )
+  summary(sfrontz)
+  
+  ### D. Trans log with z_it (formtz) --------------------------------------
+  sfronttz = sfa( formtz,  
+                  data = CSH_panel,
+                  ineffDecrease = F, # FALSE for cost function and TRUE for production
+                  truncNorm = FALSE, # FALSE -> errors have half normal distribution TRUE -> truncated distribution (mu parameter is added)
+                  timeEffect = FALSE, # time is allowed to have an effect on efficiency
+                  printIter = 1 )
+  summary(sfronttz)
+  
+# 7. Azores Prediction: Dummy Frontier -------------------------------------
+  ## 6.1 Prediction --------------------------------------------------------
+  CSH_predict = CSH_panel |> select(hospital, year, operational)
+  
+  # A. prediction tables
+  prediction_sfa = data.frame(predict = exp(predict(sfront, newdata = CSH_panel)),
+                              hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predict_sfa" = "predict")
+  prediction_sfat = data.frame(predict = exp(predict(sfrontt, newdata = CSH_panel)),
+                               hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predict_sfat" = "predict")
+  prediction_sfaz = data.frame(predict = exp(predict(sfrontz, newdata = CSH_panel)),
+                               hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predict_sfaz" = "predict")
+  prediction_sfatz = data.frame(predict = exp(predict(sfronttz, newdata = CSH_panel)),
+                                hospital = rownames(data.frame(predict(sfront, newdata = CSH_panel)))) |>
+    separate("hospital", c("hospital", "year"),sep = "-") |>
+    rename("predict_sfatz" = "predict")
+  
+  # B. Join all data frames 
+  list_df = list(CSH_predict, prediction_sfa,
+                 prediction_sfat, prediction_sfaz, prediction_sfatz)
+  
+  CSH_predict = list_df |> 
+    reduce(function(x, y) full_join(x, y, by = c("hospital" = "hospital",
+                                                 "year" = "year"), 
+                                    .init = NULL))
+  # C. Differences
+  # These differences are too high
+  CSH_predict = CSH_predict |> mutate(difference = operational - predict_sfa, differencet = operational - predict_sfat,
+                                          differencez = operational - predict_sfaz, differencetz = operational - predict_sfatz,
+                                      difference_perc_sfa = (operational - predict_sfa)/predict_sfa,
+                                      difference_perc_sfat = (operational - predict_sfat)/predict_sfat,
+                                      difference_perc_sfaz = (operational - predict_sfaz)/predict_sfaz,
+                                      difference_perc_sfatz = (operational - predict_sfatz)/predict_sfatz)
+  write.csv(CSH_predict, "CSH_predict.csv", row.names = FALSE)
+  
+  ## 6.2 Efficiency --------------------------------------------------------
+  
+  # A. Efficiency tables
+  efficiencies_sfa = data.frame(efficiency = efficiencies(sfront, newdata = CSH_panel),
+                                hospital = rownames(efficiencies(sfront, newdata = CSH_panel))) |>
+    rename("efficiency_sfa" = "efficiency")
+  efficiencies_sfat = data.frame(efficiency = efficiencies(sfrontt, newdata = CSH_panel),
+                                 hospital = rownames(efficiencies(sfrontt, newdata = CSH_panel))) |>
+    rename("efficiency_sfat" = "efficiency")
+  
+  efficiencies_sfaz = data.frame(hospital = rownames(efficiencies(sfrontz, newdata = CSH_panel)),
+                                 efficiency = efficiencies(sfrontz, newdata = CSH_panel)) |>
+    pivot_longer(c("efficiency.2015","efficiency.2016", "efficiency.2017", "efficiency.2018",
+                   "efficiency.2019", "efficiency.2020", "efficiency.2021", "efficiency.2022"),
+                 names_to = "year", values_to = "efficiency_sfa_z") |>
+     mutate( year = str_remove(year, ".*\\.")) # .(any character) *(zero or more occurences) \\. (".")
+    
+  
+  efficiencies_sfatz = data.frame(efficiency = efficiencies(sfronttz, newdata = CSH_panel),
+                                  hospital = rownames(efficiencies(sfronttz, newdata = CSH_panel))) |>
+    pivot_longer(c("efficiency.2015","efficiency.2016", "efficiency.2017", "efficiency.2018",
+                   "efficiency.2019", "efficiency.2020", "efficiency.2021", "efficiency.2022"),
+                 names_to = "year", values_to = "efficiency_sfa_tz") |>
+    mutate( year = str_remove(year, ".*\\."))
+  
+  # B. Join all efficiency tables
+  list_df = list(efficiencies_sfa, efficiencies_sfat,
+                 efficiencies_sfaz, efficiencies_sfatz)
+  
+  CSH_effi = full_join(efficiencies_sfaz, efficiencies_sfatz, 
+                       by = c("hospital" = "hospital", "year" = "year"))
+  CSH_effi = bind_rows(CSH_effi, efficiencies_sfa,efficiencies_sfat)
+  
+  write.csv(CSH_effi, "CSH_effi.csv", row.names = FALSE)
+  
   # §. Tests --------------------------------------------------------------
 warnings()  
-setwd("../..")
+setwd("../../..")
 ###### END #####
 rm(list=ls())
